8a4e0c0cb44843ada01a9ae7ff1aa34c86e747e2
Sam Jones <sam@smjdesign.co.uk>
2024-05-07 15:34:00 +0100

    Updating service with AddWeekdays method, replacing other private methods

--- Moonpig.PostOffice.Api/Services/DespatchService.cs
+++ Moonpig.PostOffice.Api/Services/DespatchService.cs
@@ -2,10 +2,8 @@
 using Moonpig.PostOffice.Api.Model;
 using Moonpig.PostOffice.Data;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace Moonpig.PostOffice.Api.Services
 {
@@ -18,53 +16,12 @@ namespace Moonpig.PostOffice.Api.Services
             _dataProvider = dataProvider ?? throw new ArgumentNullException(nameof(dataProvider));
         }
 
-        //public DespatchDate GetDespatchDates(List<int> productIds, DateTime orderDate)
-        //{
-        //    var maxLeadTime = orderDate; // max lead time
-        //    foreach (var id in productIds)
-        //    {
-        //        var product = _dataProvider.GetProduct(id);
-        //        var supplierId = product.SupplierId;
-
-        //        var supplier = _dataProvider.GetSupplier(supplierId);
-        //        var leadTime = supplier.LeadTime;
-
-        //        if (orderDate.AddDays(leadTime) > maxLeadTime)
-        //        {
-        //            maxLeadTime = orderDate.AddDays(leadTime);
-        //        }
-        //    }
-        //    if (maxLeadTime.DayOfWeek == DayOfWeek.Saturday)
-        //    {
-        //        return new DespatchDate { Date = maxLeadTime.AddDays(2) };
-        //    }
-        //    else if (maxLeadTime.DayOfWeek == DayOfWeek.Sunday)
-        //    {
-        //        return new DespatchDate { Date = maxLeadTime.AddDays(1) };
-        //    }
-
-        //    else return new DespatchDate { Date = maxLeadTime };
-        //}
-
         public DespatchDate GetDespatchDates(List<int> productIds, DateTime orderDate)
         {
             var maxLeadTime = GetMaxLeadTime(productIds);
+            var dispatchDate = AddWeekdays(orderDate, maxLeadTime);
 
-            if (orderDate.DayOfWeek == DayOfWeek.Friday)
-            {
-                var daysUntilMonday = GetDaysUntilMonday(orderDate);
-                var leadTime = GetDaysUntilMonday(orderDate) + maxLeadTime;
-                return new DespatchDate(orderDate.AddDays(leadTime));
-            }
-            if (orderDate.DayOfWeek == DayOfWeek.Saturday || orderDate.DayOfWeek == DayOfWeek.Sunday)
-            {
-                var daysUntilMonday = GetDaysUntilMonday(orderDate);
-                return new DespatchDate(orderDate.AddDays(daysUntilMonday + maxLeadTime));
-            }
-            else
-            {
-                return new DespatchDate(orderDate.AddDays(maxLeadTime));
-            };
+            return new DespatchDate(dispatchDate);
         }
 
         private int GetMaxLeadTime(List<int> productIds)
@@ -83,28 +40,6 @@ namespace Moonpig.PostOffice.Api.Services
             }
 
             return leadTimes.Max();
-        }
-
-        private static int GetDaysUntilMonday(DateTime date) => (DayOfWeek.Monday - date.DayOfWeek + 7) % 7;
-
-        private static int GetWeekendDaysBetweenDates(DateTime startDate, DateTime endDate)
-        {
-            var dates = Enumerable.Range(0, 1 + endDate.Subtract(startDate).Days)
-                .Select(offset => startDate.AddDays(offset))
-                .ToArray();
-
-            var weekendDays = dates.Where(x => x.DayOfWeek == DayOfWeek.Saturday || x.DayOfWeek == DayOfWeek.Sunday);
-            return weekendDays.Count();
-        }
-
-        private static int GetWeekDaysBetweenDates(DateTime startDate, DateTime endDate)
-        {
-            var dates = Enumerable.Range(0, 1 + endDate.Subtract(startDate).Days)
-                .Select(offset => startDate.AddDays(offset))
-                .ToArray();
-
-            var weekendDays = dates.Where(x => x.DayOfWeek != DayOfWeek.Saturday || x.DayOfWeek != DayOfWeek.Sunday);
-            return weekendDays.Count();
         }
 
         private static DateTime AddWeekdays(DateTime startDate, int weekdaysToAdd)
